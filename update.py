b51e3140ae737fd9c1388d3b5d941ad93fe475bc
Ali Ahmet <44195146+aliahmetggg@users.noreply.github.com>
2022-04-24 11:01:59 +0300

    Delete update.py

--- update.py
+++
@@ -1,9 +0,0 @@
-from ilk.models import Book
-
-# Create your tests here.
-def update():
-
-    Book.objects.filter(id= 6).update(author = 'berkuysal')
-    Book.objects.filter(id= 6).update(stock_count = 100)
-
-update()
