6c31a559abb56b05adafb51cd231c7aafe191335
Ali Ahmet <44195146+aliahmetggg@users.noreply.github.com>
2022-04-24 11:01:32 +0300

    Delete kitaplik directory

--- kitaplik/read.py
+++
@@ -1,9 +0,0 @@
-from ilk.models import Book
-
-# Create your tests here.
-def read():
-
-    for i in Book.objects.all():
-        print(i.id, i.book_name, i.author, i.stock_count, i.prize, sep='-')
-
-read()
